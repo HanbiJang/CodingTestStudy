f94702e583d3eb46605ee25ec367f134ccb8cad7
HanbiJang <hanbijang.dev@gmail.com>
2022-08-06 15:32:10 +0900

    34 버블소트

--- Inflean_Lecture/Lecture/MySolution/34/main.cpp
+++ Inflean_Lecture/Lecture/MySolution/34/main.cpp
@@ -13,29 +13,10 @@ int main() {
 		scanf("%d", &arr[i] );
 	}
 	
-    /*
-    vector<int> a;
-	vector<int> b;
-	
-	for (int i=0; i<N; i++){
-		if(arr[i] > 0){
-			a.push_back(arr[i]);
-		}
-		else{
-			b.push_back(arr[i]);
-		}
-	}	
-	
-	for (int i=0; i<b.size(); i++){
-		printf("%d ", b[i]);
-	} 
-	*/
-	
-	// 방법 [2] 
-	// 버블 정렬 이용하기 (양수,음수 순서면 바꾸기) 
+	// 버블 정렬
 	for(int i=0; i<N-1; i++){
 		for(int j=0; j<N-i-1; j++){
-			if(arr[j]>0 && arr[j+1]<0){
+			if(arr[j]>arr[j+1]){
 				int tmp=arr[j];
 				arr[j]=arr[j+1];
 				arr[j+1]=tmp;
