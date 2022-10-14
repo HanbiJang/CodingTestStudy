750712912887f29a78ffc3a261fc603c7bdf1add
HanbiJang <hanbijang.dev@gmail.com>
2022-10-14 20:02:49 +0900

    실전 모의고사 2-3, 2-4

--- Inflean_Lecture/실전 모의고사/MySolution/2-3/main.cpp
+++ Inflean_Lecture/실전 모의고사/MySolution/2-3/main.cpp
@@ -1,22 +1,66 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
+
 int main() {
-	vector<int> arr;
-	int N,K; //작업의 개수 , 정전이 발생한 시간 
+	
+	long long N,K; //작업의 개수 , 정전이 발생한 시간 
 	
 	cin>>N;
-	for(int i=0; i<N; i++){
-		cin>>arr[i];
+	vector<long long> arr(N+1);
+	vector<long long> arr_(N+1); //노정렬 
+	for(long long i=1; i<=N; i++){
+		long long n;
+		cin>>n;
+		arr[i]=n;
+		arr_[i]=n;
 	}
 	cin>>K; 
 	
-	//멀티태스킹
+	sort(arr.begin(), arr.end());
 	
+	//멀티태스킹
+	long long i = 1; //진행 중인 테스트 인덱스
+	long long time_ = K; //남은 시간 	
+	long long task = arr.size()-1; //남은 작업
+
+	while(i<=N){ //한바퀴 돌리기 
+		long long tmp = (arr[i] - arr[i-1]) * task;
+		
+		//결과 적용
+		if(time_ - tmp < 0) {
+			break;
+		}
+		
+		else {
+			time_ -= tmp;
+			task--;
+			
+			if(task<=0) {
+				cout<<"-1";
+				exit(0);
+			}				
+		}	
+		i++;
+	}
+		
+	int cnt=0;
+	long long target = time_ % task; //(양수)남은 시간 % 남은 작업 (앞에서부터 target번째 작업에서 종료됨)***
+
+	for(long long j=1; j<=N; j++){ //순회 
+		//i :마지막으로 진행 완료한 테스트 인덱스
+		if(arr[i]<=arr_[j]){ //다음 테스크의 조건 : 이미 종료된 테스크보다 크기가 크거나 같아야 함 
+			if(target==cnt ) {
+				cout<<j;
+				break;
+			}
+			cnt++;
+		}	
+	}
 	
 	return 0;
 }
-
--- Inflean_Lecture/실전 모의고사/MySolution/2-4/main.cpp
+++ Inflean_Lecture/실전 모의고사/MySolution/2-4/main.cpp
@@ -5,95 +5,117 @@
 
 using namespace std;
 
-int map[1001][1001];
+/*
+ 0 : 영희 이동 가능
+ 1: 영희 이동 불가
+ 2 영희 시작 위치
+ 3: 기사 위치 (단 하나)
+ 4 : 산딸기 위치 (여러개)
+산딸기얻어서 기사까지 가는 최단거리 출력
+*/
+
+int map[1001][1001]; //원본
+int map1[1001][1001]; //딸기 ***
+int map2[1001][1001]; //기사 ***
 int ch[1001][1001];
 
-// 0 : 영희 이동 가능
-// 1: 영희 이동 불가
-// 2 영희 시작 위치
-// 3: 기사 위치
-// 4 : 산딸기 위치 
+int x[] = { 0,0,1,-1 };
+int y[] = { 1,-1,0,0 };
 
-//산딸기->기사 가는 최단거리 
+int W, H; //열,행
 
-int x[]={0,0,1,-1};
-int y[]={1,-1,0,0};
-
-int cnt;
-
-int main() {
-	
-	int W,H; //열,행
-	cin>>W>>H;
-	int startr, startc;
-	for(int i=1; i<=H; i++){
-		for(int j=1; j<=W; j++){
-			cin>>map[i][j];
-			
-			if(map[i][j]==2){
-				startr = i;
-				startc = j;
-			}
+void clear() {
+	for (int i = 0; i <= H; i++) {
+		for (int j = 0; j <= W; j++) {
+			ch[i][j] = 0;
 		}
-	}	 
-	
-	//산딸기
-	bool isGet = false;
-	int tmp=0;
-	
-	queue<pair<int,int> > q;
-	q.push({startr, startc});
-	while(!q.empty()){
+	}
+	return;
+}
+
+void BFS ( int map_[1001][1001],int startr, int startc) {
+	clear();
+	queue<pair<int, int> > q;
+	q.push(make_pair(startr, startc));
+
+	while (!q.empty()) {
 		int curr = q.front().first;
 		int curc = q.front().second;
 		q.pop();
-		
-		if(map[curr][curc]==3 && isGet){ //최종 목적 달성
-			cnt+=tmp; 
-			cout<<cnt; //출력 
-			break;
-		}
-		
-		if(map[curr][curc]==4){ //산딸기
-			isGet=true;
-			cnt+=tmp;
-			tmp=0;
-			cout<<cnt<<endl;
-			//ch클리어
-			for(int i=1; i<=H; i++){
-				for(int j=1; j<=W; j++){
-					cout<<map[i][j]<<" ";
-				}	
-				cout<<endl;
-			}
-			ch[curr][curc] = 4;
-			for(int i=1; i<=H; i++){
-				for(int j=1; j<=W; j++){
-					cout<<ch[i][j]<<" ";
-				}	
-				cout<<endl;
-			}			 
-		}
-		
-		for(int i=0; i<4; i++){
+
+		for (int i = 0; i < 4; i++) {
 			int nextr = curr + x[i];
 			int nextc = curc + y[i];
-			
-			if(nextr<1 || nextc<1 || nextr>H || nextc>W
-			|| map[nextr][nextc]==1 || (map[nextr][nextc]==3 && isGet==false)
-			) continue;
-			
-			if(ch[nextr][nextc]==0){				 
-				ch[nextr][nextc]=1;
-				q.push(make_pair(nextr,nextc));
-				tmp++;
-				break; //한번 선택		
-			}
-		}		
-	} 
-	
 
+			if (nextr<1 || nextc<1 || nextr>H || nextc>W || map[nextr][nextc]==1) continue;
+
+			if (ch[nextr][nextc] == 0) {
+				ch[nextr][nextc] = 1;
+				q.push(make_pair(nextr, nextc));
+				map_[nextr][nextc] = map_[curr][curc] + 1;
+			}
+		}
+	}
+
+	return;
+}
+
+
+
+int main() {
+
+	cin >> W >> H;
+	int startr, startc;
+	int nstartr, nstartc;
+	for (int i = 1; i <= H; i++) {
+		for (int j = 1; j <= W; j++) {
+			cin >> map[i][j];
+
+			if (map[i][j] == 2) {
+				startr = i;
+				startc = j;
+			}
+			if (map[i][j] == 3) { //기사 위치 
+				nstartr = i;
+				nstartc = j;
+			}
+		}
+	}
+
+	//(1) 시작지점 ~ 다른 지점
+	map1[startr][startc]=0; //영희 지점	
+	BFS(map1, startr, startc);
+
+	//(2) 기사
+	map2[nstartr][nstartc]=0; //기사 지점
+	BFS(map2, nstartr, nstartc);
 	
+	int tmp;
+	int min_=2147000000;
+	
+	for (int i = 1; i <= H; i++) {
+		tmp=min_;
+		for (int j = 1; j <= W; j++) {
+			int rr1,cc1;
+			if(map[i][j]==4 && map1[i][j]!=0 && map2[i][j]!=0){ //산딸기 
+				rr1=i;
+				cc1=j;
+				
+				tmp = map1[rr1][cc1] + map2[rr1][cc1];
+				if(tmp < min_) min_ = tmp;
+			}		
+		}
+	}
+	
+	cout<<min_;
+	
+//	for (int i = 1; i <= H; i++) {
+//		for (int j = 1; j <= W; j++) {
+//			cout<<map2[i][j]<<" ";	
+//		}
+//		cout<<endl;
+//	}
+
 	return 0;
 }
 
