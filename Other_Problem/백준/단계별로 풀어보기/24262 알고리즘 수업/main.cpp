4d6a603cd4f29235dee0248efa29a34215a7bb25
HanbiJang <hanbijang.dev@gmail.com>
2023-10-03 00:41:08 +0900

    24262 알고리즘 수업

---
+++ Other_Problem/백준/단계별로 풀어보기/24262 알고리즘 수업/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+//요약: 입력 크기 n이 주어지면, 코드1의  수행 횟수와 다항식의 최고차항의 차수를  출력하라 
+//
+/*
+	MenOfPassion(A[], n) {
+    	i = n / 2;
+    	return A[i]; # 코드1
+	}
+
+*/
+//재귀문이 아니므로 코드1의 수행횟수는 1번
+//다항식이 상수시간이므로 최고차항의 차수는 0이다  
+
+int main(){
+	
+	ios_base::sync_with_stdio(false);
+	
+	int n;
+	cin>>n;
+	
+	cout<<1<<endl;
+	cout<<0;
+
+
+	return 0;
+}
+
