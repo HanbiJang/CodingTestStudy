c5963d8e58f7c3d9ca310d2a78c8121df8c64aa7
HanbiJang <hanbijang.dev@gmail.com>
2023-10-05 02:18:19 +0900

    단계별로 풀어보기 24264 알고리즘 수업 3

---
+++ Other_Problem/백준/단계별로 풀어보기/24264 알고리즘 수업 - 알고리즘의 수행 시간 3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	ios_base::sync_with_stdio(false);
+
+	long long n; // 입력n의 최대값이 500,000 이기때문에 n*n을 출력하기위함 
+	cin>>n;
+	
+	cout<<n*n<<endl;
+	cout<<2;
+
+
+	
+	return 0;
+}
+
