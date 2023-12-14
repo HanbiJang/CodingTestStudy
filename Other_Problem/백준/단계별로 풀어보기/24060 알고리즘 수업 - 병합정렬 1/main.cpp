ca013ba29c0241a2fe6f9bdd689ede2d991721a0
HanbiJang <hanbijang.dev@gmail.com>
2023-12-14 20:20:07 +0900

    24060 알고리즘 수업 - 병합정렬 1

--- Other_Problem/백준/단계별로 풀어보기/24060 알고리즘 수업 - 병합정렬 1/main.cpp
+++ Other_Problem/백준/단계별로 풀어보기/24060 알고리즘 수업 - 병합정렬 1/main.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+int saveCnt=0;
+int K;
+
 void merge(vector<int> &A, int p, int q, int r) {
     int i = p;
     int j = q + 1;
@@ -25,34 +28,19 @@ void merge(vector<int> &A, int p, int q, int r) {
     i = p;
     t = 1;
 
-    while (i <= r)
-        A[i++] = tmp[t++];
+    while (i <= r){ //정렬된 결과를 원래 배열 A에 다시 저장
+		A[i++] = tmp[t++];
+		saveCnt++;
+    	if(saveCnt==K) cout<<tmp[t-1];
+	}
 }
 
 void merge_sort(vector<int> &A, int p, int r) {
     if (p < r) {
         int q = (p + r) / 2;
-        merge_sort(A, p, q);
-		
-		for(auto i : A){
-			cout<<i<<" ";			
-		}
-		cout<<"\n";
-				
-        merge_sort(A, q + 1, r);
-		
-		for(auto i : A){
-			cout<<i<<" ";			
-		}
-		cout<<"\n";
-						
-        merge(A, p, q, r);
-		
-		for(auto i : A){
-			cout<<i<<" ";			
-		}
-		cout<<"\n";
-				         
+        merge_sort(A, p, q);				
+        merge_sort(A, q + 1, r);					
+        merge(A, p, q, r);				         
     }
 }
 
@@ -65,7 +53,7 @@ int main(){
 	cin>>N;
 	
 	//저장횟수 K
-	int K;
+	
 	cin>>K; 
 	
 	vector<int> arr;
@@ -80,6 +68,8 @@ int main(){
 	//배열 A에 K번째 저장되는 수를 출력하라
 	//저장회수가 K보다 작으면 -1 출력하라
 	merge_sort(arr,0,arr.size()-1);
+	
+	if(saveCnt<K) cout<<-1;
 		
 	return 0;
 }
