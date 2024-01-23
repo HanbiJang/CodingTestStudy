81f981f963f1432fccaf0bfe2c0ee04f22ac0c70
HanbiJang <hanbijang.dev@gmail.com>
2024-01-23 16:04:02 +0900

    2563 색종이 만들기

--- Other_Problem/백준/단계별로 풀어보기/2563 색종이/main.cpp
+++ Other_Problem/백준/단계별로 풀어보기/2563 색종이/main.cpp
@@ -1,37 +1,72 @@
 #include <iostream>
-
+#include <vector> 
 using namespace std;
 
+void countColoredPapers(vector<vector<int>>& paper, int& whiteCount, int& blueCount, int row, int col, int size) {
+    //종이 크기가 1일 때
+    if (size == 1) {
+        // 해당 칸이 흰색이면 흰색 종이 수 증가, 아니면 파란색 종이 수 증가
+        if (paper[row][col] == 0) {
+            whiteCount++;
+        } else {
+            blueCount++;
+        }
+        return;
+    }
 
-int arr[101][101];
-int main(){
-	
-	ios_base::sync_with_stdio(false);
-	
-	int num; //색종이 개수 
-	cin>>num;
-	
-	for(int i=0; i<num; i++){
-		int a,b;
-		cin>>a>>b;
-		
-		for(int j=0; j<10; j++){
-			for(int m=0; m<10; m++){
-				arr[a+j][b+m] = 1;
-			}
-		}
-		
-	}
-	
-	int res = 0;
-	for(int j=1; j<=100; j++){
-		for(int m=1; m<=100; m++){
-			if(arr[j][m]==1) res++;
-		}
-	}
-	
-	cout<<res;
-		
-	return 0;
+    //종이들이 모두 같은 색인지 확인
+    bool isAllSameColor = true;
+    int firstColor = paper[row][col];
+    
+    //작은 종이 내부를 돌면서 같은 색인지 확인
+    for (int i = row; i < row + size; i++) {
+        for (int j = col; j < col + size; j++) {
+            if (paper[i][j] != firstColor) {
+                isAllSameColor = false;
+                break;
+            }
+        }
+        if (!isAllSameColor) {
+            break;
+        }
+    }
+
+    //종이들이 모두 같은 색이라면 해당 색의 종이 수 증가
+    //아니라면 네 등분으로 자르고 재귀적으로 호출
+    if (isAllSameColor) {
+        if (firstColor == 0) {
+            whiteCount++;
+        } else {
+            blueCount++;
+        }
+    } else {
+        int newSize = size / 2;
+        countColoredPapers(paper, whiteCount, blueCount, row, col, newSize);
+        countColoredPapers(paper, whiteCount, blueCount, row, col + newSize, newSize);
+        countColoredPapers(paper, whiteCount, blueCount, row + newSize, col, newSize);
+        countColoredPapers(paper, whiteCount, blueCount, row + newSize, col + newSize, newSize);
+    }
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    //NxN 크기의 종이
+    vector<vector<int>> paper(N, vector<int>(N, 0));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> paper[i][j];
+        }
+    }
+
+    int whiteCount = 0, blueCount = 0;
+
+    countColoredPapers(paper, whiteCount, blueCount, 0, 0, N);
+
+    cout << whiteCount << endl;
+    cout << blueCount << endl;
+
+    return 0;
 }
 
